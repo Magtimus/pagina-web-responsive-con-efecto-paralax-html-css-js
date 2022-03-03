7ba675a477523e8073fe9e00439d0b30e6341672
Magtimus <cristiancedano1030@gmail.com>
2022-03-02 23:59:09 -0400

    pagina-web-responsive-con-efecto-paralax-html-css-js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+
+    document.querySelector(".menu").classList.toggle("mostrar_menu");
+    
+}
+
+window.onscroll = function () {
+    var posicion = window.pageYOffset || document.documentElement.scrollTop;
+    var elemento1 = document.getElementById("icon_heart");
+    var elemento2 = document.getElementById("icon_fire");
+    elemento1.style.bottom = posicion * 0.1 + "px";
+    elemento2.style.top = posicion * 0.1 + "px";
+}
